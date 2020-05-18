8ae52754f645cc6712ed8d8fc0c4e279ae349017
Peng Xu <robotpengxu@gmail.com>
2020-05-17 17:36:14 -0700

    done: format.cpp; check Up Time dispaly

--- src/format.cpp
+++ src/format.cpp
@@ -3,9 +3,17 @@
 #include "format.h"
 
 using std::string;
+using std::to_string;
 
-// TODO: Complete this helper function
+// --TODO--: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  string elapsed = "";
+  long hh, mm, ss;
+  hh = seconds / 3600;
+  mm = (seconds - hh*3600) / 60;
+  ss = seconds - hh*3600 - mm*60;
+  return to_string(hh) + ":" + to_string(mm) + ":" + to_string(ss);
+}
